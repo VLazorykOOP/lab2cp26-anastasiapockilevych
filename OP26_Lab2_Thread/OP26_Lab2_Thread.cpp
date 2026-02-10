a93693f2f3ec6ebbd3e6447a865cc10fda369010
anastasiapockilevych <pokhilevych.anastasiia@chnu.edu.ua>
2026-02-10 13:44:16 +0300

    code added

--- OP26_Lab2_Thread/OP26_Lab2_Thread.cpp
+++ OP26_Lab2_Thread/OP26_Lab2_Thread.cpp
@@ -1,20 +1,104 @@
-// OP26_Lab2_Thread.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+const int WIDTH = 50;
+const int HEIGHT = 20;
+const double SPEED = 2.0;
+const int DELAY_MS = 100;
+const int N_SECONDS = 3;
+
+mutex printMutex;
+
+class Rabbit {
+public:
+    string type;
+    double x, y;
+    double vx, vy;
+
+    Rabbit(string type, double startX, double startY)
+        : type(type), x(startX), y(startY), vx(0), vy(0) {
+    }
+
+    void liveAlbino() {
+        double currentSpeed = SPEED;
+
+        while (true) {
+            {
+                lock_guard<mutex> lock(printMutex);
+                cout << "[ALBINO] " << type << " at X=" << (int)x << endl;
+            }
+
+            x += currentSpeed;
+
+            if (x >= WIDTH || x <= 0) {
+                currentSpeed *= -1;
+            }
+
+            this_thread::sleep_for(chrono::milliseconds(DELAY_MS));
+        }
+    }
+
+    void liveOrdinary() {
+        changeDirection();
+
+        int stepsForChange = (N_SECONDS * 1000) / DELAY_MS;
+        int counter = 0;
+
+        while (true) {
+            {
+                lock_guard<mutex> lock(printMutex);
+                cout << "[ORDINARY] " << type << " at (" << (int)x << ", " << (int)y << ")" << endl;
+            }
+
+            x += vx;
+            y += vy;
+
+            if (x <= 0 || x >= WIDTH) { x = (x <= 0 ? 0 : WIDTH); vx *= -1; }
+            if (y <= 0 || y >= HEIGHT) { y = (y <= 0 ? 0 : HEIGHT); vy *= -1; }
+
+            counter++;
+            if (counter >= stepsForChange) {
+                {
+                    lock_guard<mutex> lock(printMutex);
+                    cout << "--- " << type << " changed random direction! ---" << endl;
+                }
+                changeDirection();
+                counter = 0;
+            }
+
+            this_thread::sleep_for(chrono::milliseconds(DELAY_MS));
+        }
+    }
+
+private:
+    void changeDirection() {
+        double angle = ((double)rand() / RAND_MAX) * 2 * 3.14159;
+        vx = SPEED * cos(angle);
+        vy = SPEED * sin(angle);
+    }
+};
+
+int main() {
+    srand(time(0));
+
+    vector<thread> threads;
+
+    threads.emplace_back(&Rabbit::liveAlbino, Rabbit("Albino_1", 0, 10));
+
+    threads.emplace_back(&Rabbit::liveOrdinary, Rabbit("Ordinary_A", 25, 10));
+    threads.emplace_back(&Rabbit::liveOrdinary, Rabbit("Ordinary_B", 10, 5));
+
+    for (auto& t : threads) {
+        if (t.joinable()) t.join();
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
